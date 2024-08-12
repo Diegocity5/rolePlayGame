81d44cf646a08087f1c24f69e0ea7e482ece3dcf
Diegocity5 <icotecc@gmail.com>
2024-08-12 18:47:32 -0500

    38: Cambiando el texto del boton1 con la propiedad innerText dinamicamente

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,7 @@ const monsterName = document.querySelector('#monsterHealth');
 const monsterHealthText = document.querySelector('#monsterHealth');
 
 function goStore(){
-    console.log("Going to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
 }
 
 function goToCave(){
