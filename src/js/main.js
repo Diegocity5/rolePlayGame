d2302b27794d7f20d13cded0812aa4fa102071a6
Diegocity5 <icotecc@gmail.com>
2024-08-13 16:50:52 -0500

    59: Agregando un segundo objecto con la misma estructura del primero pero con los valores de la funcion goStore

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,12 @@ const locations = [
         "button functions": [goStore, goToCave, fightDragon],
         text: "You are in the town square. You see a sign that says \"Store\"."
     },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)","Buy weapon (30 gold)","Go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."
+    }
 ];
 
 function update(location){
