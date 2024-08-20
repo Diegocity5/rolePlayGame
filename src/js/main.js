c6b33e6360b9399e7c56d7fd360d34078c6bfe50
Diegocity5 <icotecc@gmail.com>
2024-08-20 17:54:58 -0500

    138: Cree una funcion que se encargara de resetear todas la variable de inicio de juego

--- src/js/main.js
+++ src/js/main.js
@@ -180,6 +180,18 @@ function defeatMonster(){
 function lose(){
     update(locations[5]);
 }
+function restart(){
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeaponIndex = 0;
+    inventory = ["stick"];
+
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
+}
 
 //initialize buttons.
 button1.onclick = goStore;
